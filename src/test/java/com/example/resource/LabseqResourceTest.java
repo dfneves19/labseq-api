6164f44e39a292110f67083772c677e0a3e8a12d
Diogo Neves <diogo.f.neves.2002@gmail.com>
2025-09-30 02:51:00 +0100

    add more unit tests

--- src/test/java/com/example/resource/LabseqResourceTest.java
+++ src/test/java/com/example/resource/LabseqResourceTest.java
@@ -22,6 +22,54 @@ public class LabseqResourceTest {
     }
 
     @Test
+    void testValueZero() {
+        given()
+            .pathParam("n", 0)
+            .when()
+                .get("/labseq/{n}")
+            .then()
+                .statusCode(200)
+                .body("n", is(0))
+                .body("value", is("0"));
+    }
+
+    @Test
+    void testValueOne() {
+        given()
+            .pathParam("n", 1)
+            .when()
+                .get("/labseq/{n}")
+            .then()
+                .statusCode(200)
+                .body("n", is(1))
+                .body("value", is("1"));
+    }
+
+    @Test
+    void testValueTwo() {
+        given()
+            .pathParam("n", 2)
+            .when()
+                .get("/labseq/{n}")
+            .then()
+                .statusCode(200)
+                .body("n", is(2))
+                .body("value", is("0"));
+    }
+
+    @Test
+    void testValueThree() {
+        given()
+        .pathParam("n", 3)
+            .when()
+                .get("/labseq/{n}")
+            .then()
+                .statusCode(200)
+                .body("n", is(3))
+                .body("value", is("1"));
+    }
+
+    @Test
     void testNonInteger() {
         given()
             .pathParam("n", "abc")
